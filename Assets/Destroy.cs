5cde1f9a75532a1f2987de1e22936d4f9107d8e0
yusakuya <yu.sakuya24@gmail.com>
2017-05-05 16:02:18 +0900

    StepAsideUnityChan 1st commit
    課題

---
+++ Assets/Destroy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //廃棄
+    void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+        Debug.Log("削除");
+    }
+}
